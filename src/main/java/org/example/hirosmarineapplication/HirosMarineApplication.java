aa6050f9a802c4cbdd04136c023375a97c787a91
Hafizh 'Abid <mkb.hafizh@upi.edu>
2024-12-05 11:06:31 +0700

    init commit

---
+++ src/main/java/org/example/hirosmarineapplication/HirosMarineApplication.java
@@ -0,0 +1,13 @@
+package org.example.hirosmarineapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirosMarineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HirosMarineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hirosmarineapplication/HirosMarineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hirosmarineapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirosMarineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
